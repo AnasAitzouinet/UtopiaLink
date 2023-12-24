dc1040cee5bc49dc9b9533da63d70db9b6343bd5
Anas AitZouinet <anasaw236@gmail.com>
2023-12-24 17:52:27 +0100

    deploy v1s

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,32 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 import { Kanit } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 const Kanits = Kanit({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700"],
 });
 
 export const metadata: Metadata = {
-  title: 'Utopia',
-  description: 'Utopia is payment gateway for the future, built on top of Stripe',
-}
+  title: "Utopia",
+  description:
+    "Utopia is payment gateway for the future, built on top of Stripe",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={`${Kanits.className }  `}>{children}</body>
+      <body className={`${Kanits.className}  `}>
+        <Analytics />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
